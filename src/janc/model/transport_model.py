eedecf005e09daa9df7344eaeb124de5edd85acc
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-27 09:57:41 +0800

    Add files via upload

---
+++ src/janc/model/transport_model.py
@@ -0,0 +1,138 @@
+"""
+The module extracts thermodynamic parameters 
+from Cantera’s database and employs dedicated functions 
+to compute critical thermodynamic state variables for gas mixtures: 
+gas constant (R), enthalpy (h), specific heat ratio (gamma) and et al.
+
+dependencies: jax & cantera(python version)
+"""
+from .LES import LES_SGS_dict
+mu_ref = 1.716e-5
+T_ref = 273.15
+S = 110.4
+nu0 = 1e-4
+Pr = 0.72
+Pr_t = 0.72
+Le = 1.0
+Le_t = 1.0
+k0 = 0.0
+D0 = 0.0
+nu_type = 'Sutherland'
+k_type = 'Prandtl number'
+D_type = 'Lewis number'
+model = 'laminar'
+LES_model = 'WALE'
+
+def set_transport(transport_config):
+    global Pr_t,Le_t,LES_model,model,nu_type, k_type, D_type, Pr, Le, nu0, k0, D0,mu_ref,T_ref,S
+    if transport_config['viscosity_model'] == 'constant':
+        nu_type = 'constant'
+        nu0 = transport_config['dynamic_viscosity']
+    elif transport_config['viscosity_model'] == 'Sutherland':
+        if 'reference_viscosity' in transport_config:
+            mu_ref = transport_config['reference_viscosity']
+        if 'reference_temprature' in transport_config:
+            T_ref = transport_config['reference_temprature']
+        if 'effective_temprature' in transport_config:
+            S = transport_config['effective_temprature']
+    else:
+        raise KeyError("Only 'Sutherland' and 'constant' viscosity model are supported.")
+    if transport_config['thermal_conductivity_model'] == 'constant':
+        k_type = 'constant'
+        k0 = transport_config['thermal_conductivity']
+    elif transport_config['thermal_conductivity_model'] == 'Prandtl number':
+        assert 'Pr' in transport_config, "Key 'Pr' must be provided"
+        Pr = transport_config['Pr']
+    else:
+        raise KeyError("Only 'Prandtl number' and 'constant' thermal conductivity model are supported.")
+    if transport_config['thermal_conductivity_model'] == 'constant':
+        k_type = 'constant'
+        k0 = transport_config['thermal_conductivity']
+    elif transport_config['thermal_conductivity_model'] == 'Prandtl number':
+        assert 'Pr' in transport_config, "Key 'Pr' must be provided"
+        Pr = transport_config['Pr']
+    else:
+        raise KeyError("Only 'Prandtl number' and 'constant' thermal conductivity model are supported.")
+    if transport_config['species_diffusivity_model'] == 'constant':
+        D_type = 'constant'
+        D0 = transport_config['species_diffusivity']
+    elif transport_config['species_diffusivity_model'] == 'Lewis number':
+        assert 'Le' in transport_config, "Key 'Le' must be provided"
+        Le = transport_config['Le']
+    else:
+        raise KeyError("Only 'Lewis number' and 'constant' species diffusivity model are supported.")
+    if 'turbulence_model' in transport_config:
+        if transport_config['turbulence_model'] == 'LES':
+            model = 'LES'
+            assert 'LES_model' in transport_config, 'Please specify the SGS model of LES.'
+            LES_model = transport_config['LES_model']
+            assert 'Pr_t' in transport_config, "Key 'Pr_t'(turbulent Prandtl number) must be provided"
+            assert 'Le_t' in transport_config, "Key 'Le_t'(turbulent Lewis number) must be provided"
+            Pr_t = transport_config['Pr_t']
+            Le_t = transport_config['Le_t']
+        elif transport_config['turbulence_model'] == 'laminar':
+            pass
+        else:
+            raise KeyError("Only 'laminar' and 'LES' model are supported.")
+
+def mu_Sutherland(rho,T):
+    mu = mu_ref * (T/T_ref)**1.5 * (T_ref+S)/(T+S)
+    return mu
+
+def mu_constant(rho,T):
+    return rho*nu0
+
+mu_dict = {'Sutherland':mu_Sutherland,
+           'constant':mu_constant}
+
+def mu_laminar(rho,T,V,dudx,dudy,dudz,dvdx,dvdy,dvdz,dwdx,dwdy,dwdz):
+    return mu_dict[nu_type](rho,T), 0
+
+def mu_LES(rho,T,V,dudx,dudy,dudz,dvdx,dvdy,dvdz,dwdx,dwdy,dwdz):
+    mu = mu_dict[nu_type](rho,T)
+    mu_t = LES_SGS_dict[LES_model](rho,V,dudx,dudy,dudz,dvdx,dvdy,dvdz,dwdx,dwdy,dwdz)
+    return mu + mu_t, mu_t
+
+total_mu_dict = {'laminar':mu_laminar,
+                 'LES':mu_LES}
+
+def mu(rho,T,V,dudx,dudy,dudz,dvdx,dvdy,dvdz,dwdx,dwdy,dwdz):
+    mu,mu_t = total_mu_dict[model](rho,T,V,dudx,dudy,dudz,dvdx,dvdy,dvdz,dwdx,dwdy,dwdz)
+    return mu, mu_t
+
+def kappa_Pr(mu,cp,mu_t):
+    return mu*cp/Pr + mu_t*cp/Pr_t
+
+def kappa_constant(mu,cp,mu_t):
+    return k0 + mu_t*cp/Pr_t
+
+kappa_dict = {'Prandtl number':kappa_Pr,
+              'constant':kappa_constant}
+
+def kappa(mu,cp,mu_t):
+    return kappa_dict[k_type](mu,cp,mu_t)
+
+def D_Le(mu,cp_i,mu_t):
+    k_i = (k_type=='Prandtl number')*mu/Pr + (k_type=='constant')*k0/cp_i
+    k_i = k_i
+    D_i = k_i/Le*cp_i
+    k_t = mu_t/Pr_t
+    D_t = k_t/Le_t*cp_i
+    return D_i+D_t
+
+def D_constant(mu,cp_i,mu_t):
+    k_t = mu_t/Pr_t
+    D_t = k_t/Le_t*cp_i
+    return D0+D_t
+
+D_dict = {'Lewis number':D_Le,
+          'constant':D_constant}
+
+def D(mu,cp_i,mu_t):
+    return D_dict[D_type](mu,cp_i,mu_t)
+
+
+
+    
+    
+    
