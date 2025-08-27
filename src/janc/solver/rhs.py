c5b73ab331fee83de3e58957cdbf53562b75d4ac
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-27 19:49:11 +0800

    Update rhs.py

--- src/janc/solver/rhs.py
+++ src/janc/solver/rhs.py
@@ -6,7 +6,7 @@ point_implicit = 'off'
 def set_rhs(thermo_config,reaction_config,flux_config,transport_config,boundary_config,source_config=None):
     global point_implicit
     thermo_model.set_thermo(thermo_config)
-    reaction_model.set_reaction(reaction_config)
+    reaction_source_terms = reaction_model.set_reaction(reaction_config)
     flux_func = flux.set_flux_solver(flux_config,transport_config)
     boundary.set_boundary(boundary_config)
     aux_func.set_source_terms(source_config)
@@ -19,10 +19,11 @@ def set_rhs(thermo_config,reaction_config,flux_config,transport_config,boundary_
     else:
         def rhs(U,aux,metrics,dt,theta):
             U_with_ghost,aux_with_ghost = boundary.boundary_conditions(U,aux,metrics,theta)
-            rhs = dt*(flux_func(U_with_ghost,aux_with_ghost,metrics) + aux_func.user_source(U,aux,theta)) + reaction_model.reaction_source_terms(U,aux,dt,theta)
+            rhs = dt*(flux_func(U_with_ghost,aux_with_ghost,metrics) + aux_func.user_source(U,aux,theta)) + reaction_source_terms(U,aux,dt,theta)
             return rhs
     return rhs
     
+
 
 
 
