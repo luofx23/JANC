fec88dc606ba8375686d352b5e28d4cc320561ad
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-27 19:33:24 +0800

    Update flux.py

--- src/janc/solver/flux.py
+++ src/janc/solver/flux.py
@@ -130,8 +130,28 @@ def set_flux_solver(flux_solver_config,transport_config=None):
         reconstruction_R_y = reconstruction_R_y_dict[interface_reconstruction]
         riemann_solver = riemann_solver_dict[riemann_solver_type]
         def advective_flux(U,aux,metrics):
-            netflux = godunov_flux(U,aux,metrics,reconstruction_L_x,reconstruction_R_x,reconstruction_L_y,reconstruction_R_y,riemann_solver)
-            return netflux
+            rho,u,v,Y,p,a = aux_func.U_to_prim(U, aux)
+            ξ_n_x,ξ_n_y = metrics['ξ-n_x'],metrics['ξ-n_y']
+            un = u*ξ_n_x + v*ξ_n_y
+            ut = -u*ξ_n_y + v*ξ_n_x
+            qx = jnp.concatenate([rho,un,ut,p,Y],axis=0)
+            η_n_x,η_n_y = metrics['η-n_x'],metrics['η-n_y']
+            un = u*η_n_x + v*η_n_y
+            ut = u*η_n_y - v*η_n_x
+            qy = jnp.concatenate([rho,ut,un,p,Y],axis=0)
+            q_L_x = reconstruction_L_x(qx)
+            q_R_x = reconstruction_R_x(qx)
+            q_L_y = reconstruction_L_y(qy)
+            q_R_y = reconstruction_R_y(qy)
+            F_interface,G_interface = riemann_solver(q_L_x,q_R_x,q_L_y,q_R_y)
+            F = jnp.concatenate([F_interface[0:1],F_interface[1:2]*ξ_n_x-F_interface[2:3]*ξ_n_y,
+                                 F_interface[1:2]*ξ_n_y+F_interface[2:3]*ξ_n_x,F_interface[3:]],axis=0)*metrics['ξ-dl']
+            G = jnp.concatenate([G_interface[0:1],G_interface[1:2]*η_n_y+G_interface[2:3]*η_n_x,
+                                 -G_interface[1:2]*η_n_x+G_interface[2:3]*η_n_y,G_interface[3:]],axis=0)*metrics['η-dl']
+            dF = F[:,1:,:]-F[:,:-1,:]
+            dG = G[:,:,1:]-G[:,:,:-1]
+            net_flux = (dF + dG)/metrics['J']
+            return -netflux
     elif solver_type == 'flux_splitting':
         interface_reconstruction = flux_solver_config['interface_reconstruction']
         split_method = flux_solver_config['split_method']
@@ -141,8 +161,26 @@ def set_flux_solver(flux_solver_config,transport_config=None):
         reconstruction_R_y = reconstruction_R_y_dict[interface_reconstruction]
         split_func = split_flux_dict[split_method]
         def advective_flux(U,aux,metrics):
-            netflux = flux_splitting(U,aux,metrics,reconstruction_L_x,reconstruction_R_x,reconstruction_L_y,reconstruction_R_y,split_func)
-            return netflux
+            ξ_n_x,ξ_n_y = metrics['ξ-n_x'],metrics['ξ-n_y']
+            Ux = jnp.concatenate([U[0:1],U[1:2]*ξ_n_x + U[2:3]*ξ_n_y, -U[1:2]*ξ_n_y + U[2:3]*ξ_n_x, U[3:]],axis=0)
+            η_n_x,η_n_y = metrics['η-n_x'],metrics['η-n_y']
+            Uy = jnp.concatenate([U[0:1],U[1:2]*η_n_y - U[2:3]*η_n_x, U[1:2]*η_n_x + U[2:3]*η_n_y, U[3:]],axis=0)
+            Fplus,Fminus = split_func(1,Ux,aux)
+            Gplus,Gminus = split_func(2,Uy,aux)
+            Fp = reconstruction_L_x(Fplus)
+            Fm = reconstruction_R_x(Fminus)
+            Gp = reconstruction_L_y(Gplus)
+            Gm = reconstruction_R_y(Gminus)
+            F_interface = Fp + Fm
+            G_interface = Gp + Gm
+            F = jnp.concatenate([F_interface[0:1],F_interface[1:2]*ξ_n_x-F_interface[2:3]*ξ_n_y,
+                                 F_interface[1:2]*ξ_n_y+F_interface[2:3]*ξ_n_x,F_interface[3:]],axis=0)*metrics['ξ-dl']
+            G = jnp.concatenate([G_interface[0:1],G_interface[1:2]*η_n_y+G_interface[2:3]*η_n_x,
+                                 -G_interface[1:2]*η_n_x+G_interface[2:3]*η_n_y,G_interface[3:]],axis=0)*metrics['η-dl']
+            dF = F[:,1:,:]-F[:,:-1,:]
+            dG = G[:,:,1:]-G[:,:,:-1]
+            net_flux = (dF + dG)/metrics['J']
+            return -netflux
     else:
         raise KeyError("JANC only support 'godunov' and 'flux_splitting'")
     
@@ -156,8 +194,59 @@ def set_flux_solver(flux_solver_config,transport_config=None):
         d_dy = d_dy_dict[viscosity_discretization]
         reconstruction_x = reconstruction_x_dict[viscosity_discretization]
         reconstruction_y = reconstruction_y_dict[viscosity_discretization]
+        def viscous_flux_node(U, aux, metrics,d_dx,d_dy):
+            ρ,u,v,Y,p,a = aux_func.U_to_prim(U,aux)
+            T = aux[1:2]
+            cp_k, _, h_k = thermo_model.get_thermo_properties(T[0])
+            cp, _, _, _, _ = thermo_model.get_thermo(T,Y)
+            Y = thermo_model.fill_Y(Y)
+            du_dξ = d_dx(u,1.0);  du_dη = d_dy(u,1.0);
+            dv_dξ = d_dx(v,1.0);  dv_dη = d_dy(v,1.0);
+            dT_dξ = d_dx(T,1.0);  dT_dη = d_dy(T,1.0);
+            dY_dξ = d_dx(Y,1.0);  dY_dη = d_dy(Y,1.0);
+            dξ_dx, dξ_dy = metrics['dξ_dx'], metrics['dξ_dy']
+            dη_dx, dη_dy = metrics['dη_dx'], metrics['dη_dy']
+            J = metrics['Jc']
+            du_dx = dξ_dx*du_dξ + dη_dx*du_dη
+            du_dy = dξ_dy*du_dξ + dη_dy*du_dη
+            dv_dx = dξ_dx*dv_dξ + dη_dx*dv_dη
+            dv_dy = dξ_dy*dv_dξ + dη_dy*dv_dη
+            dT_dx = dξ_dx*dT_dξ + dη_dx*dT_dη
+            dT_dy = dξ_dy*dT_dξ + dη_dy*dT_dη
+            dY_dx = dξ_dx*dY_dξ + dη_dx*dY_dη
+            dY_dy = dξ_dy*dY_dξ + dη_dy*dY_dη
+            mu,mu_t = transport_model.mu(ρ,T,J,du_dx,du_dy,None,dv_dx,dv_dy,None,None,None,None)
+            k = transport_model.kappa(mu, cp, mu_t)
+            D_k = transport_model.D(mu,cp_k,mu_t)
+            λ = -2/3*mu
+            S11 = du_dx; S22 = dv_dy;
+            S12 = 0.5 * (du_dy + dv_dx)
+            div_u = S11 + S22
+            τ_xx = 2*mu*du_dx + λ*div_u
+            τ_yy = 2*mu*dv_dy + λ*div_u
+            τ_xy = 2*mu*S12
+            qx = -k * dT_dx
+            qy = -k * dT_dy
+            jx =  - ρ * D_k * dY_dx
+            jy =  - ρ * D_k * dY_dy
+            ex = jnp.sum(jx*h_k,axis=0,keepdims=True)
+            ey = jnp.sum(jy*h_k,axis=0,keepdims=True)
+            Fv = jnp.concatenate([jnp.zeros_like(ρ),
+                                 τ_xx, τ_xy,
+                                 u*τ_xx + v*τ_xy - qx - ex, -jx[0:-1]], axis=0)
+            Gv = jnp.concatenate([jnp.zeros_like(ρ),
+                                 τ_xy, τ_yy,
+                                 u*τ_xy + v*τ_yy - qy - ey, -jy[0:-1]], axis=0)
+            F_hat = J * (dξ_dx*Fv + dξ_dy*Gv)
+            G_hat = J * (dη_dx*Fv + dη_dy*Gv)
+            return F_hat, G_hat
         def viscous_flux(U,aux,metrics):
-            netflux = viscous_flux_face(U, aux, metrics, d_dx, d_dy, reconstruction_x, reconstruction_y)
+            F_hat, G_hat = viscous_flux_node(U, aux, metrics, d_dx, d_dy)
+            F_interface = reconstruction_x(F_hat)
+            G_interface = reconstruction_y(G_hat)
+            dF = F_interface[:,1:,:]-F_interface[:,:-1,:]
+            dG = G_interface[:,:,1:]-G_interface[:,:,:-1]
+            net_flux = (dF + dG)/metrics['J']
             return netflux
         def total_flux(U,aux,metrics):
             total_flux = advective_flux(U,aux,metrics) + viscous_flux(U,aux,metrics)
@@ -166,3 +255,4 @@ def set_flux_solver(flux_solver_config,transport_config=None):
         total_flux = advective_flux
     
     return total_flux
+
