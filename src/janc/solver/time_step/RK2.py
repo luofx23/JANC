ea6ddac159dc841ded9dfa99c33b9c81299d1564
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-27 18:35:27 +0800

    Add files via upload

--- src/janc/solver/time_step/RK2.py
+++ src/janc/solver/time_step/RK2.py
@@ -1,9 +1,9 @@
 import jax.numpy as jnp
-from .. import rhs,aux_func
+from .. import aux_func
 
-def advance_one_step(U,aux,metrics,dt,theta):
-    U1 = U + rhs.rhs(U,aux,metrics,dt,theta)
+def advance_one_step(U,aux,metrics,dt,theta,rhs):
+    U1 = U + rhs(U,aux,metrics,dt,theta)
     aux1 = aux_func.update_aux(U1, aux)
-    U2 = U + 1/2*(rhs.rhs(U,aux,metrics,dt,theta)+rhs.rhs(U1,aux1,metrics,dt,theta))
+    U2 = U + 1/2*(rhs(U,aux,metrics,dt,theta)+rhs(U1,aux1,metrics,dt,theta))
     aux2 = aux_func.update_aux(U2, aux1)
     return U2,aux2
