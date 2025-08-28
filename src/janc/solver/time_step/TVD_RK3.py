da36fde98c99af243de6ab842ac711eccb21631d
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-28 14:11:08 +0800

    Add files via upload

--- src/janc/solver/time_step/TVD_RK3.py
+++ src/janc/solver/time_step/TVD_RK3.py
@@ -1,12 +1,12 @@
 import jax.numpy as jnp
-from .. import aux_func
+from .. import rhs,aux_func
 
-def advance_one_step(U,aux,metrics,dt,theta,rhs):
-    U1 = U + rhs(U,aux,metrics,dt,theta)
+def advance_one_step(U,aux,metrics,dt,theta):
+    U1 = U + rhs.rhs(U,aux,metrics,dt,theta)
     aux1 = aux_func.update_aux(U1, aux)
-    U2 = 3/4*U + 1/4 * (U1 + rhs(U1,aux1,metrics,dt,theta))
+    U2 = 3/4*U + 1/4 * (U1 + rhs.rhs(U1,aux1,metrics,dt,theta))
     aux2 = aux_func.update_aux(U2, aux1)
-    U3 = 1/3*U + 2/3 * (U2 + rhs(U2,aux2,metrics,dt,theta))
+    U3 = 1/3*U + 2/3 * (U2 + rhs.rhs(U2,aux2,metrics,dt,theta))
     aux3 = aux_func.update_aux(U3, aux2)
     return U3,aux3
 
