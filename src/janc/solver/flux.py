e0e35b4f8747a9230eab092859bd34a6a329a70f
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-28 10:29:51 +0800

    Update flux.py

--- src/janc/solver/flux.py
+++ src/janc/solver/flux.py
@@ -161,8 +161,6 @@ def set_flux_solver(flux_solver_config,transport_config=None):
         reconstruction_R_y = reconstruction_R_y_dict[interface_reconstruction]
         split_func = split_flux_dict[split_method]
         def advective_flux(U,aux,metrics):
-            dx = 1e-4
-            dy = 1e-4
             #ξ_n_x,ξ_n_y = metrics['ξ-n_x'],metrics['ξ-n_y']
             #Ux = jnp.concatenate([U[0:1],U[1:2]*ξ_n_x + U[2:3]*ξ_n_y, -U[1:2]*ξ_n_y + U[2:3]*ξ_n_x, U[3:]],axis=0)
             #η_n_x,η_n_y = metrics['η-n_x'],metrics['η-n_y']
@@ -179,9 +177,9 @@ def set_flux_solver(flux_solver_config,transport_config=None):
                                  #F_interface[1:2]*ξ_n_y+F_interface[2:3]*ξ_n_x,F_interface[3:]],axis=0)*metrics['ξ-dl']
             #G = jnp.concatenate([G_interface[0:1],G_interface[1:2]*η_n_y+G_interface[2:3]*η_n_x,
                                  #-G_interface[1:2]*η_n_x+G_interface[2:3]*η_n_y,G_interface[3:]],axis=0)*metrics['η-dl']
-            dF = (F_interface[:,1:,:]-F_interface[:,:-1,:])*dy
-            dG = (G_interface[:,:,1:]-G_interface[:,:,:-1])*dx
-            net_flux = (dF + dG)/(dx*dy)#metrics['J']
+            dF = (F_interface[:,1:,:]-F_interface[:,:-1,:])*metrics['ξ-dl']
+            dG = (G_interface[:,:,1:]-G_interface[:,:,:-1])*metrics['η-dl']
+            net_flux = (dF + dG)/metrics['J']
             return -net_flux
     else:
         raise KeyError("JANC only support 'godunov' and 'flux_splitting'")
@@ -257,6 +255,7 @@ def set_flux_solver(flux_solver_config,transport_config=None):
         total_flux = advective_flux
     
     return total_flux
+
 
 
 
