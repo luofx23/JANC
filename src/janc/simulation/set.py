647aa313179f680205c0071e183a91bb5ce602ee
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-29 16:33:44 +0800

    Add files via upload

--- src/janc/simulation/set.py
+++ src/janc/simulation/set.py
@@ -1,9 +1,12 @@
 from jax import jit
 import jax.numpy as jnp
-from ..solver import rhs,time_step,aux_func
+from ..solver_1D import rhs,time_step,aux_func as rhs_1D,time_step_1D,aux_func_1D
+from ..solver_2D import rhs,time_step,aux_func as rhs_2D,time_step_2D,aux_func_2D
+from ..solver_3D import rhs,time_step,aux_func as rhs_3D,time_step_3D,aux_func_3D
 from ..model import reaction_model
 
 def set_simulation(simulation_config):
+    dim = simulation_config['dimension']
     thermo_config = simulation_config['thermo_config']
     reaction_config = simulation_config['reaction_config']
     if 'transport_config' in simulation_config:
@@ -16,7 +19,19 @@ def set_simulation(simulation_config):
         source_config = simulation_config['source_config']
     else:
         source_config = None
-    rhs.set_rhs(thermo_config, reaction_config, flux_config, transport_config, boundary_config, source_config)
+    if dim == '1D':
+        rhs = rhs_1D
+        time_step = time_step_1D
+        aux_func = aux_func_1D
+    if dim == '2D':
+        rhs = rhs_2D
+        time_step = time_step_2D
+        aux_func = aux_func_2D
+    if dim == '3D':
+        rhs = rhs_3D
+        time_step = time_step_3D
+        aux_func = aux_func_3D
+    rhs.set_rhs(dim, thermo_config, reaction_config, flux_config, transport_config, boundary_config, source_config)
     time_scheme = simulation_config['temporal_evolution_scheme']
     if reaction_config['is_detailed_chemistry']:
         @jit
