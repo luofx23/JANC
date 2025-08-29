f406268092c54a691896b4252b8ac9c228d3e9c9
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-29 16:26:10 +0800

    Add files via upload

---
+++ src/janc/boundary/boundary_conditions_2D/__init__.py
@@ -0,0 +1,22 @@
+from . import neumann,no_slip_wall,pressure_outlet,slip_wall
+
+
+left_bd_dict = {'neumann':neumann.left,
+                'no_slip_wall':no_slip_wall.left,
+                'pressure_outlet':pressure_outlet.left,
+                'slip_wall':slip_wall.left}
+
+right_bd_dict = {'neumann':neumann.right,
+                'no_slip_wall':no_slip_wall.right,
+                'pressure_outlet':pressure_outlet.right,
+                'slip_wall':slip_wall.right}
+
+top_bd_dict = {'neumann':neumann.top,
+                'no_slip_wall':no_slip_wall.top,
+                'pressure_outlet':pressure_outlet.top,
+                'slip_wall':slip_wall.top}
+
+bottom_bd_dict = {'neumann':neumann.bottom,
+                'no_slip_wall':no_slip_wall.bottom,
+                'pressure_outlet':pressure_outlet.bottom,
+                'slip_wall':slip_wall.bottom}
