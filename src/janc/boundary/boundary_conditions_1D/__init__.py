e50c8b90a5e81e4e480a02a4c2090d1c0e0af40e
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-08-30 14:41:15 +0800

    Update __init__.py

--- src/janc/boundary/boundary_conditions_1D/__init__.py
+++ src/janc/boundary/boundary_conditions_1D/__init__.py
@@ -1,13 +1,14 @@
 from . import neumann,no_slip_wall,pressure_outlet,slip_wall
 
 
-left_bd_dict = {'neumann':neumann.left,
+left_bd_dict_1D = {'neumann':neumann.left,
                 'no_slip_wall':no_slip_wall.left,
                 'pressure_outlet':pressure_outlet.left,
                 'slip_wall':slip_wall.left}
 
-right_bd_dict = {'neumann':neumann.right,
+right_bd_dict_1D = {'neumann':neumann.right,
                 'no_slip_wall':no_slip_wall.right,
                 'pressure_outlet':pressure_outlet.right,
                 'slip_wall':slip_wall.right}
+
 
