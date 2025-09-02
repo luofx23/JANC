9ff534d4ed40cf8ef6ccad6559a9885c260f9b3c
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-02 10:08:27 +0800

    Update LF.py

--- src/janc/solver_1D/flux_splitting/LF.py
+++ src/janc/solver_1D/flux_splitting/LF.py
@@ -8,7 +8,7 @@ def split_flux(U, aux, metrics):
     dξ_dx = metrics['dξ_dx']
     J = metrics['Jc']
     zx = dξ_dx
-    F = jnp.concatenate([rho * u, rho * u ** 2 + p, u * (rhoE + p), rho * u * Y], axis=0)
+    F = zx*jnp.concatenate([rho * u, rho * u ** 2 + p, u * (rhoE + p), rho * u * Y], axis=0)
     um = zx*jnp.nanmax(abs(u) + a)
     Hplus = 0.5 * J * (F + um * U)
     Hminus = 0.5 * J * (F - um * U)
