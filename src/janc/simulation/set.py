8a6bfd0f26c118f35b2271c8e0f98a6b49aa2252
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-03 13:17:40 +0800

    Update set.py

--- src/janc/simulation/set.py
+++ src/janc/simulation/set.py
@@ -54,15 +54,14 @@ def set_simulation(simulation_config):
             U, aux = time_step.time_step_dict[time_scheme](U,aux,metrics,dt,theta)
             U1 = U + reaction_model.reaction_source_terms(U,aux,dt,theta)
             aux1 = aux_func.update_aux(U1, aux)
-            U2 = 3/4*U + 1/4 * (U1 + reaction_model.reaction_source_terms(U1,aux1,dt,theta))
+            U2 = U + 1/2*(reaction_model.reaction_source_terms(U,aux,dt,theta)+reaction_model.reaction_source_terms(U1,aux1,dt,theta))
             aux2 = aux_func.update_aux(U2, aux1)
-            U3 = 1/3*U + 2/3 * (U2 + reaction_model.reaction_source_terms(U2,aux2,dt,theta))
-            aux3 = aux_func.update_aux(U3, aux2)
-            return U3,aux3
+            return U2,aux2
     return advance_one_step
             
 
     
+
 
 
 
