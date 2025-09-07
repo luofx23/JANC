22760c5ca1ff5c15b7d219c31828948f926e9159
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-07 18:43:21 +0800

    Update boundary_padding.py

--- src/janc_easy/boundary/boundary_padding.py
+++ src/janc_easy/boundary/boundary_padding.py
@@ -1,36 +1,31 @@
 import jax
 import jax.numpy as jnp
 
-def pad(U,aux):
-    field = jnp.concatenate([U,aux],axis=0)
+def pad(U):
+    field = U
     field_periodic_x = jnp.concatenate([field[:,-4:-3,:],field[:,-3:-2,:],field[:,-2:-1,:],field,field[:,1:2,:],field[:,2:3,:],field[:,3:4,:]],axis=1)
     field_periodic_pad = jnp.concatenate([field_periodic_x[:,:,-4:-3],field_periodic_x[:,:,-3:-2],field_periodic_x[:,:,-2:-1],field_periodic_x,field_periodic_x[:,:,1:2],field_periodic_x[:,:,2:3],field_periodic_x[:,:,3:4]],axis=2)
-    U_periodic_pad,aux_periodic_pad = field_periodic_pad[0:-2],field_periodic_pad[-2:]
-    return U_periodic_pad,aux_periodic_pad
+    return field_periodic_pad
 
-def replace_lb(U_bd, aux_bd, padded_U, padded_aux):
+def replace_lb(U_bd, padded_U):
     U = padded_U.at[:,0:3,3:-3].set(U_bd)
-    aux = padded_aux.at[:,0:3,3:-3].set(aux_bd)
-    return U, aux
+    return U
 
     
-def replace_rb(U_bd, aux_bd, padded_U, padded_aux):
+def replace_rb(U_bd, padded_U):
     U = padded_U.at[:,-3:,3:-3].set(U_bd)
-    aux = padded_aux.at[:,-3:,3:-3].set(aux_bd)
-    return U, aux
+    return U
 
 
-def replace_ub(U_bd, aux_bd, padded_U, padded_aux):
+def replace_ub(U_bd, padded_U):
     U = padded_U.at[:,3:-3,-3:].set(U_bd)
-    aux = padded_aux.at[:,3:-3,-3:].set(aux_bd)
-    return U, aux
+    return U
 
 
 
-def replace_bb(U_bd, aux_bd, padded_U, padded_aux):  
+def replace_bb(U_bd, padded_U):  
     U = padded_U.at[:,3:-3,0:3].set(U_bd)
-    aux = padded_aux.at[:,3:-3,0:3].set(aux_bd)
-    return U, aux
+    return U
 
 
 
@@ -53,10 +48,10 @@ def exchange_halo(device_grid):
     return new_grid
 
 
-def parallel_pad(U,aux):
-    field = jnp.concatenate([U,aux],axis=0)
+def parallel_pad(U):
+    field = U
     field_periodic_x = exchange_halo(field)
     field_periodic_pad = jnp.concatenate([field_periodic_x[:,:,-4:-3],field_periodic_x[:,:,-3:-2],field_periodic_x[:,:,-2:-1],field_periodic_x,field_periodic_x[:,:,1:2],field_periodic_x[:,:,2:3],field_periodic_x[:,:,3:4]],axis=2)
-    U_periodic_pad,aux_periodic_pad = field_periodic_pad[0:-2],field_periodic_pad[-2:]
-    return U_periodic_pad,aux_periodic_pad
+    return field_periodic_pad
     
+
