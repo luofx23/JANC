862a7c0a0ee6bfbb88dc4aae9e2149c0a2f1cabf
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 09:46:36 +0800

    Update boundary.py

--- src/janc_v2/boundary/boundary.py
+++ src/janc_v2/boundary/boundary.py
@@ -8,83 +8,84 @@ boundary_func = {'left_boundary':None,
                  'top_boundary':None,
                  'bottom_boundary':None}
 
-def set_boundary(boundary_config:dict,dim:str):
+def set_boundary(boundary_config:dict):
     global  boundary_func  
     if callable(boundary_config['left_boundary']):
-        def left_boundary(padded_U,padded_aux,metrics,theta=None):
+        def left_boundary(padded_U,padded_aux,theta=None):
             U_lb,aux_lb = padded_U[:,3:6,3:-3],padded_aux[:,3:6,3:-3]
-            U_lb,aux_lb = boundary_config['left_boundary'](U_lb,aux_lb,metrics,theta)
+            U_lb,aux_lb = boundary_config['left_boundary'](U_lb,aux_lb,theta)
             U_with_lb,aux_with_lb = replace_lb_2D(U_lb,aux_lb,padded_U,padded_aux)
             return U_with_lb,aux_with_lb
     elif boundary_config['left_boundary']=='periodic':
-        def left_boundary(padded_U,padded_aux,metrics,theta=None):
+        def left_boundary(padded_U,padded_aux,theta=None):
             return padded_U,padded_aux
     else:
-         def left_boundary(padded_U,padded_aux,metrics,theta=None):
+         def left_boundary(padded_U,padded_aux,theta=None):
              U_lb,aux_lb = padded_U[:,3:6,3:-3],padded_aux[:,3:6,3:-3]
-             U_lb,aux_lb = left_bd_dict_2D[boundary_config['left_boundary']](U_lb,aux_lb,metrics,theta)
+             U_lb,aux_lb = left_bd_dict_2D[boundary_config['left_boundary']](U_lb,aux_lb,theta)
              U_with_lb,aux_with_lb = replace_lb_2D(U_lb,aux_lb,padded_U,padded_aux)
              return U_with_lb,aux_with_lb   
     boundary_func['left_boundary'] = left_boundary
     
     if callable(boundary_config['right_boundary']):
-        def right_boundary(padded_U,padded_aux,metrics,theta=None):
+        def right_boundary(padded_U,padded_aux,theta=None):
             U_rb,aux_rb = padded_U[:,-6:-3,3:-3],padded_aux[:,-6:-3,3:-3]
-            U_rb,aux_rb = boundary_config['right_boundary'](U_rb,aux_rb,metrics,theta)
+            U_rb,aux_rb = boundary_config['right_boundary'](U_rb,aux_rb,theta)
             U_with_rb,aux_with_rb = replace_rb_2D(U_rb,aux_rb,padded_U,padded_aux)
             return U_with_rb,aux_with_rb
     elif boundary_config['right_boundary']=='periodic':
-        def right_boundary(padded_U,padded_aux,metrics,theta=None):
+        def right_boundary(padded_U,padded_aux,theta=None):
             return padded_U,padded_aux
     else:
-        def right_boundary(padded_U,padded_aux,metrics,theta=None):
+        def right_boundary(padded_U,padded_aux,theta=None):
             U_rb,aux_rb = padded_U[:,-6:-3,3:-3],padded_aux[:,-6:-3,3:-3]
-            U_rb,aux_rb = right_bd_dict_2D[boundary_config['right_boundary']](U_rb,aux_rb,metrics,theta)
+            U_rb,aux_rb = right_bd_dict_2D[boundary_config['right_boundary']](U_rb,aux_rb,theta)
             U_with_rb,aux_with_rb = replace_rb_2D(U_rb,aux_rb,padded_U,padded_aux)
             return U_with_rb,aux_with_rb        
     boundary_func['right_boundary'] = right_boundary    
     
     if callable(boundary_config['bottom_boundary']):
-        def bottom_boundary(padded_U,padded_aux,metrics,theta=None):
+        def bottom_boundary(padded_U,padded_aux,theta=None):
             U_bb,aux_bb = padded_U[:,3:-3,3:6],padded_aux[:,3:-3,3:6]
-            U_bb,aux_bb = boundary_config['bottom_boundary'](U_bb,aux_bb,metrics,theta)
+            U_bb,aux_bb = boundary_config['bottom_boundary'](U_bb,aux_bb,theta)
             U_with_bb,aux_with_bb = replace_bb_2D(U_bb,aux_bb,padded_U,padded_aux)
             return U_with_bb,aux_with_bb
     elif boundary_config['bottom_boundary']=='periodic':
-        def bottom_boundary(padded_U,padded_aux,metrics,theta=None):
+        def bottom_boundary(padded_U,padded_aux,theta=None):
             return padded_U,padded_aux
     else:
-        def bottom_boundary(padded_U,padded_aux,metrics,theta=None):
+        def bottom_boundary(padded_U,padded_aux,theta=None):
             U_bb,aux_bb = padded_U[:,3:-3,3:6],padded_aux[:,3:-3,3:6]
-            U_bb,aux_bb = bottom_bd_dict_2D[boundary_config['bottom_boundary']](U_bb,aux_bb,metrics,theta)
+            U_bb,aux_bb = bottom_bd_dict_2D[boundary_config['bottom_boundary']](U_bb,aux_bb,theta)
             U_with_bb,aux_with_bb = replace_bb_2D(U_bb,aux_bb,padded_U,padded_aux)
             return U_with_bb,aux_with_bb
     boundary_func['bottom_boundary'] = bottom_boundary
     
     if callable(boundary_config['top_boundary']):
-        def top_boundary(padded_U,padded_aux,metrics,theta=None):
+        def top_boundary(padded_U,padded_aux,theta=None):
             U_ub,aux_ub = padded_U[:,3:-3,-6:-3],padded_aux[:,3:-3,-6:-3]
-            U_ub,aux_ub = boundary_config['top_boundary'](U_ub,aux_ub,metrics,theta)
+            U_ub,aux_ub = boundary_config['top_boundary'](U_ub,aux_ub,theta)
             U_with_ub,aux_with_ub = replace_ub_2D(U_ub,aux_ub,padded_U,padded_aux)
             return U_with_ub,aux_with_ub
     elif boundary_config['top_boundary'] == 'periodic':
-        def top_boundary(padded_U,padded_aux,metrics,theta=None):
+        def top_boundary(padded_U,padded_aux,theta=None):
             return padded_U,padded_aux
     else:
-        def top_boundary(padded_U,padded_aux,metrics,theta=None):
+        def top_boundary(padded_U,padded_aux,theta=None):
             U_ub,aux_ub = padded_U[:,3:-3,-6:-3],padded_aux[:,3:-3,-6:-3]
-            U_ub,aux_ub = top_bd_dict_2D[boundary_config['top_boundary']](U_ub,aux_ub,metrics,theta)
+            U_ub,aux_ub = top_bd_dict_2D[boundary_config['top_boundary']](U_ub,aux_ub,theta)
             U_with_ub,aux_with_ub = replace_ub_2D(U_ub,aux_ub,padded_U,padded_aux)
             return U_with_ub,aux_with_ub
     boundary_func['top_boundary'] = top_boundary
         
-def boundary_conditions_2D(U, aux, metrics, theta=None):
+def boundary_conditions_2D(U, aux, theta=None):
     U_periodic_pad,aux_periodic_pad = pad_2D(U,aux)
-    U_with_lb,aux_with_lb = boundary_func['left_boundary'](U_periodic_pad,aux_periodic_pad, metrics, theta)
-    U_with_rb,aux_with_rb = boundary_func['right_boundary'](U_with_lb,aux_with_lb,metrics,theta)
-    U_with_bb,aux_with_bb = boundary_func['bottom_boundary'](U_with_rb,aux_with_rb,metrics,theta)
-    U_with_ghost_cell,aux_with_ghost_cell = boundary_func['top_boundary'](U_with_bb,aux_with_bb,metrics,theta)
+    U_with_lb,aux_with_lb = boundary_func['left_boundary'](U_periodic_pad,aux_periodic_pad, theta)
+    U_with_rb,aux_with_rb = boundary_func['right_boundary'](U_with_lb,aux_with_lb,theta)
+    U_with_bb,aux_with_bb = boundary_func['bottom_boundary'](U_with_rb,aux_with_rb,theta)
+    U_with_ghost_cell,aux_with_ghost_cell = boundary_func['top_boundary'](U_with_bb,aux_with_bb,theta)
     return U_with_ghost_cell,aux_with_ghost_cell
+
 
 
 
