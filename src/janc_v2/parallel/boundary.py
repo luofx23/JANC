227bceca5af0e1be8960348e61cad52184e78c30
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 09:56:24 +0800

    Update boundary.py

--- src/janc_v2/parallel/boundary.py
+++ src/janc_v2/parallel/boundary.py
@@ -1,6 +1,6 @@
 import jax
 import jax.numpy as jnp
-from .boundary_padding import pad
+from .boundary_padding import pad_2D
 from ..boundary import boundary
 
 ##parallel settings##
@@ -12,9 +12,10 @@ boundary_func = boundary.boundary_func
 
 def boundary_conditions(U,aux,theta=None):
     device_idx = jax.lax.axis_index('x')
-    U_periodic_pad,aux_periodic_pad = pad(U,aux)
+    U_periodic_pad,aux_periodic_pad = pad_2D(U,aux)
     U_with_lb,aux_with_lb = jax.lax.cond(device_idx==0,lambda:boundary_func['left_boundary'](U_periodic_pad,aux_periodic_pad,theta),lambda:(U_periodic_pad,aux_periodic_pad))
     U_with_rb,aux_with_rb = jax.lax.cond(device_idx==(num_devices-1),lambda:boundary_func['right_boundary'](U_with_lb,aux_with_lb,theta),lambda:(U_with_lb,aux_with_lb))
     U_with_bb,aux_with_bb = boundary_func['bottom_boundary'](U_with_rb,aux_with_rb,theta)
-    U_with_ghost_cell,aux_with_ghost_cell = boundary_func['up_boundary'](U_with_bb,aux_with_bb,theta)
+    U_with_ghost_cell,aux_with_ghost_cell = boundary_func['top_boundary'](U_with_bb,aux_with_bb,theta)
     return U_with_ghost_cell,aux_with_ghost_cell
+
