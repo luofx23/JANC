5aee14e021cd539f0ea8a52922f2856f55bdb5f8
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 10:53:03 +0800

    Add flux splitting and Godunov time step dictionaries

--- src/janc_v2/solver/time_step/__init__.py
+++ src/janc_v2/solver/time_step/__init__.py
@@ -1,6 +1,12 @@
 from . import RK2,TVD_RK3
 
-time_step_dict = {
-    "RK2": RK2.advance_one_step,
-    "TVD_RK3": TVD_RK3.advance_one_step
+time_step_dict_flux_splitting = {
+    "RK2": RK2.advance_one_step_flux_splitting,
+    "TVD_RK3": TVD_RK3.advance_one_step_splitting
 }
+
+time_step_dict_flux_godunov = {
+    "RK2": RK2.advance_one_step_godunov,
+    "TVD_RK3": TVD_RK3.advance_one_step_godunov
+}
+
