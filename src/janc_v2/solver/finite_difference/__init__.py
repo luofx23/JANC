86bbfe0da7e226877e734a68f35a5890f9bbd0ea
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 17:08:11 +0800

    Delete src/janc_v2/solver directory

--- src/janc_v2/solver/finite_difference/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import CENTRAL6
-
-d_dx_dict = {
-    "CENTRAL6": CENTRAL6.d_dx
-}
-
-d_dy_dict = {
-    "CENTRAL6": CENTRAL6.d_dy
-}
--- src/janc_v2/solver/flux_splitting/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import LF, SW
-
-split_flux_dict = {
-    "LF": LF.split_flux,
-    "SW": SW.split_flux
-}
