ecea15ab2b2c01b964343b9f4ad6f58b681507b5
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 20:53:01 +0800

    Update RK2.py

--- src/janc_v2/simulation/time_step/RK2.py
+++ src/janc_v2/simulation/time_step/RK2.py
@@ -16,14 +16,14 @@ def advance_one_step_amr(level, blk_data, dx, dy, dt, ref_blk_data, ref_blk_info
     U,aux = ghost_blk_data[:,0:-2],ghost_blk_data[:,-2:]
     dev = rhs_func(U,aux,dx,dy,dt,theta)
     U1 = U + dev
-    aux1 = vmap(update_func,in_axes=(0,0))(U1, aux)
+    aux1 = update_func(U1, aux)
     blk_data1 = jnp.concatenate([U1,aux1],axis=1)
     blk_data1 = amr.update_external_boundary(level, blk_data, blk_data1[..., 3:-3, 3:-3], ref_blk_info)
     ghost_blk_data1 = amr.get_ghost_block_data(blk_data1, ref_blk_info)
     U1, aux1 = ghost_blk_data1[:,0:-2],ghost_blk_data1[:,-2:]
     dev1 = rhs_func(U1,aux1,dx,dy,dt,theta)
     U2 = U + 1/2*(dev + dev1)
-    aux2 = vmap(update_func,in_axes=(0,0))(U2, aux1)
+    aux2 = update_func(U2, aux1)
     blk_data2 = jnp.concatenate([U2,aux2],axis=1)
     blk_data2 = amr.update_external_boundary(level, blk_data, blk_data2[..., 3:-3, 3:-3], ref_blk_info)
     return blk_data2
