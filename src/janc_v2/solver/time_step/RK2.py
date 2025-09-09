cfb9c1b5a9d2887e4892303e646de554ee769d48
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 10:49:36 +0800

    Update RK2.py

--- src/janc_v2/solver/time_step/RK2.py
+++ src/janc_v2/solver/time_step/RK2.py
@@ -1,23 +1,29 @@
 import jax.numpy as jnp
 from .. import rhs,aux_func
 
-def advance_one_step_flux_splitting(U,aux,metrics,dt,theta):
-    U1 = U + (rhs.rhs_flux(U,aux,metrics,dt,theta)+rhs.rhs_source(U,aux,metrics,dt,theta))
+def advance_one_step_flux_splitting(U,aux,dx,dy,dt,theta):
+    dev = rhs.rhs_flux(U,aux,dx,dy,dt,theta)+rhs.rhs_source(U,aux,dt,theta)
+    U1 = U + dev
     aux1 = aux_func.update_aux(U1, aux)
-    U2 = U + 1/2*(rhs.rhs_flux(U,aux,metrics,dt,theta)+rhs.rhs_source(U,aux,metrics,dt,theta))
+    dev1 = rhs.rhs_flux(U1,aux1,dx,dy,dt,theta)+rhs.rhs_source(U1,aux1,dt,theta)
+    U2 = U + 1/2*(dev + dev1)
     aux2 = aux_func.update_aux(U2, aux1)
     return U2,aux2
 
-def advance_one_step_godunov(U,aux,metrics,dt,theta):
-    U1 = U + rhs.rhs_flux(U,aux,metrics,dt,theta)
+def advance_one_step_godunov(U,aux,dx,dy,dt,theta):
+    dev = rhs.rhs_flux(U,aux,dx,dy,dt,theta)
+    U1 = U + dev
     aux1 = aux_func.update_aux(U1, aux)
-    U2 = U + 1/2*rhs.rhs_flux(U,aux,metrics,dt,theta)
+    dev1 = rhs.rhs_flux(U1,aux1,dx,dy,dt,theta)
+    U2 = U + 1/2*(dev + dev1)
     aux2 = aux_func.update_aux(U2, aux1)
     
     U, aux = U2,aux2
     
-    U1 = U + rhs.rhs_source(U,aux,metrics,dt,theta)
+    dev = rhs.rhs_source(U,aux,dt,theta)
+    U1 = U + dev
     aux1 = aux_func.update_aux(U1, aux)
-    U2 = U + 1/2*rhs.rhs_source(U,aux,metrics,dt,theta)
+    dev1 = rhs.rhs_source(U1,aux1,dt,theta)
+    U2 = U + 1/2*(dev + dev1)
     aux2 = aux_func.update_aux(U2, aux1)
     return U2,aux2
