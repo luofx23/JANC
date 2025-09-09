25cb7956b64e843fe47e6381a7d7501e47faaaf8
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 10:41:33 +0800

    Update RK2.py

--- src/janc_v2/solver/time_step/RK2.py
+++ src/janc_v2/solver/time_step/RK2.py
@@ -1,10 +1,23 @@
 import jax.numpy as jnp
 from .. import rhs,aux_func
-#from ...model import reaction_model
 
-def advance_one_step(U,aux,metrics,dt,theta):
-    U1 = U + rhs.rhs(U,aux,metrics,dt,theta)
+def advance_one_step_flux_splitting(U,aux,metrics,dt,theta):
+    U1 = U + (rhs.rhs_flux(U,aux,metrics,dt,theta)+rhs.rhs_source(U,aux,metrics,dt,theta))
     aux1 = aux_func.update_aux(U1, aux)
-    U2 = U + 1/2*(rhs.rhs(U,aux,metrics,dt,theta)+rhs.rhs(U1,aux1,metrics,dt,theta))
+    U2 = U + 1/2*(rhs.rhs_flux(U,aux,metrics,dt,theta)+rhs.rhs_source(U,aux,metrics,dt,theta))
+    aux2 = aux_func.update_aux(U2, aux1)
+    return U2,aux2
+
+def advance_one_step_godunov(U,aux,metrics,dt,theta):
+    U1 = U + rhs.rhs_flux(U,aux,metrics,dt,theta)
+    aux1 = aux_func.update_aux(U1, aux)
+    U2 = U + 1/2*rhs.rhs_flux(U,aux,metrics,dt,theta)
+    aux2 = aux_func.update_aux(U2, aux1)
+    
+    U, aux = U2,aux2
+    
+    U1 = U + rhs.rhs_source(U,aux,metrics,dt,theta)
+    aux1 = aux_func.update_aux(U1, aux)
+    U2 = U + 1/2*rhs.rhs_source(U,aux,metrics,dt,theta)
     aux2 = aux_func.update_aux(U2, aux1)
     return U2,aux2
