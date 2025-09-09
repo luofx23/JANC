d55ce7e13b734aa629314b7b7a1542d94ed88186
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-09 15:02:20 +0800

    Update TVD_RK3.py

--- src/janc_v2/solver/time_step/TVD_RK3.py
+++ src/janc_v2/solver/time_step/TVD_RK3.py
@@ -63,14 +63,14 @@ def advance_one_step_godunov_amr(level, blk_data, dx, dy, dt, ref_blk_data, ref_
 
     ghost_blk_data1 = amr.get_ghost_block_data(blk_data1, ref_blk_info)
     U1,aux1 = ghost_blk_data1[:,0:-2],ghost_blk_data1[:,-2:]
-    U2 = 3/4*U + 1/4*(U1 + rhs.rhs_flux_amr(U1, aux1, dx, dy, dt, theta)
+    U2 = 3/4*U + 1/4*(U1 + rhs.rhs_flux_amr(U1, aux1, dx, dy, dt, theta))
     aux2 = vmap(aux_func.update_aux,in_axes=(0,0))(U2, aux1)
     blk_data2 = jnp.concatenate([U2,aux2],axis=1)
     blk_data2 = amr.update_external_boundary(level, blk_data, blk_data2[..., 3:-3, 3:-3], ref_blk_info)
 
     ghost_blk_data2 = amr.get_ghost_block_data(blk_data2, ref_blk_info)
     U2,aux2 = ghost_blk_data2[:,0:-2],ghost_blk_data2[:,-2:]
-    U3 = 1/3*U + 2/3*(U2 + rhs.rhs_flux_amr(U2, aux2, dx, dy, dt, theta)
+    U3 = 1/3*U + 2/3*(U2 + rhs.rhs_flux_amr(U2, aux2, dx, dy, dt, theta))
     aux3 = vmap(aux_func.update_aux,in_axes=(0,0))(U3, aux2)
     blk_data3 = jnp.concatenate([U3,aux3],axis=1)
     blk_data3 = amr.update_external_boundary(level, blk_data, blk_data3[..., 3:-3, 3:-3], ref_blk_info)
