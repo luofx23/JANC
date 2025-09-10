7b37d421ef5740c15b72e1cd737af15220dc5497
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-10 18:59:57 +0800

    Delete src/janc_v2/solver_2D/time_step directory

--- src/janc_v2/solver_2D/time_step/RK2.py
+++
@@ -1,9 +0,0 @@
-import jax.numpy as jnp
-from .. import rhs,aux_func
-
-def advance_one_step(U,aux,dx,dy,dt,theta):
-    U1 = U + rhs.rhs(U,aux,dx,dy,dt,theta)
-    aux1 = aux_func.update_aux(U1, aux)
-    U2 = U + 1/2*(rhs.rhs(U,aux,dx,dy,dt,theta)+rhs.rhs(U1,aux1,dx,dy,dt,theta))
-    aux2 = aux_func.update_aux(U2, aux1)
-    return U2,aux2
