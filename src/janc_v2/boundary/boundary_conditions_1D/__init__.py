39b3c3deb1b365b5ee0d1aec1329f641b04bcefd
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-22 15:16:41 +0800

    Update __init__.py

--- src/janc_v2/boundary/boundary_conditions_1D/__init__.py
+++ src/janc_v2/boundary/boundary_conditions_1D/__init__.py
@@ -1,14 +1,15 @@
-from . import neumann,no_slip_wall,pressure_outlet,slip_wall
+from . import zero_gradient,no_slip_wall,pressure_outlet,slip_wall
 
 
-left_bd_dict_1D = {'neumann':neumann.left,
+left_bd_dict_1D = {'zero_gradient':zero_gradient.left,
                 'no_slip_wall':no_slip_wall.left,
                 'pressure_outlet':pressure_outlet.left,
                 'slip_wall':slip_wall.left}
 
-right_bd_dict_1D = {'neumann':neumann.right,
+right_bd_dict_1D = {'zero_gradient':zero_gradient.right,
                 'no_slip_wall':no_slip_wall.right,
                 'pressure_outlet':pressure_outlet.right,
                 'slip_wall':slip_wall.right}
+
 
 
